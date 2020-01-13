d39a0f69f064029a1aa4bc7ddd6c4c9fc23b598a
joaoh <joao_camargo10@hotmail.com>
2020-01-13 20:34:52 -0300

    Creating header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createMuiTheme } from '@material-ui/core/styles'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import AppBar from '@material-ui/core/AppBar'
 import './App.css';
 
 const theme = createMuiTheme({
@@ -17,9 +18,15 @@ const theme = createMuiTheme({
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
+      <AppBar position='static' color='primary'>
+        <div className='header'>
+          <p className='title'>Shopping List</p>
+        </div>
+      </AppBar>
       <div className="App">
         <h1>Test</h1>
       </div>
+
     </MuiThemeProvider>
   );
 }
