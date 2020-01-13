1bb259a27bb9022294c50432ddba4acfce925532
joaoh <joao_camargo10@hotmail.com>
2020-01-12 23:16:18 -0300

    Theme and theme provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#282c34'
+    },
+    secondary: {
+      main: '#61dafb'
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <h1>Test</h1>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
