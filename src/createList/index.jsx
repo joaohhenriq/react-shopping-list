84e2a57a4fd8036cf58b81f70a2c666490fb309b
joaoh <joao_camargo10@hotmail.com>
2020-01-14 20:17:50 -0300

    list style

--- src/createList/index.jsx
+++ src/createList/index.jsx
@@ -1,50 +1,58 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem'
+import Button from '@material-ui/core/Button'
 import { InputAdornment } from '@material-ui/core';
+
+import './list.css'
 
 const units = ['kg', 'lt', 'un']
 
 const CreateList = props => (
     <div className='page-container'>
         <form className='form-container'>
-            <TextField
-                name='list'
-                label="List"
-                value={''}
-                required
-            />
-            <TextField
-                name='product'
-                label="Product"
-                value={''}
-                required
-            />
-            <TextField
-                name='quantity'
-                label="Quantity"
-                value={''}
-                required
-            />
-            <TextField
-                select
-                name='unity'
-                label="Unity"
-                value={''}
-                required
-            >
-                {units.map((option) => (
-                    <MenuItem key={option}>{option}</MenuItem>
-                ))}
-            </TextField>
-            <TextField
-                name='price'
-                label="Price"
-                value={''}
-                InputProps={{
-                    startAdornment: <InputAdornment position='start'>R$</InputAdornment>
-                }}
-            />
+            <div className='form-row'>
+                <TextField
+                    name='list'
+                    label="List"
+                    value={''}
+                    required
+                />
+                <Button variant='outlined' color='secondary'>Add</Button>
+            </div>
+            <div className='form-row'>
+                <TextField
+                    name='product'
+                    label="Product"
+                    value={''}
+                    required
+                />
+                <TextField
+                    name='quantity'
+                    label="Quantity"
+                    value={''}
+                    required
+                />
+                <TextField
+                    select
+                    name='unity'
+                    label="Unity"
+                    value={''}
+                    required
+                >
+                    {units.map((option) => (
+                        <MenuItem key={option}>{option}</MenuItem>
+                    ))}
+                </TextField>
+                <TextField
+                    name='price'
+                    label="Price"
+                    value={''}
+                    InputProps={{
+                        startAdornment: <InputAdornment position='start'>R$</InputAdornment>
+                    }}
+                />
+            </div>
         </form>
     </div>
 )
